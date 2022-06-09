cd832f7d52168a9669697b0f30f2f412113b5c37
fadildillah <fadillahm704@gmail.com>
2022-06-09 19:36:32 +0700

    minor fix

--- no-3/jsonConvert.js
+++ no-3/jsonConvert.js
@@ -1,38 +1,34 @@
-const obj = {
-    'address.street': 'Jalan sakura',
-    'address.provice': 'banten',
-    'address.city': 'tangerang',
-    'person.name': 'john',
-    'person.email': 'john@doe.com',
-    'userType': 'admin',
-    'accessLevel': 'QA'
-}
+// object yang diberikan
+const givenObject = {
+    "address.street" : "jalan sakura",
+    "address.provice" : "banten",
+    "address.city" : "tangerang",
+    "person.name" : "john",
+    "person.email" : "john@doe.com",
+    "userType" : "admin",
+    "accessLevel" : "QA",
+};
 
+// function manual
+function parseNestedJSON(obj) {
+    const result = {};
 
-function jsonConvert(obj) {
-    let address = {};
-    let person = {};
-    let userType = {};
-    let accessLevel = {};
-    for (let key in obj) {
-        if (key.includes('address')) {
-            address[key.split('.')[1]] = obj[key];
-        } else if (key.includes('person')) {
-            person[key.split('.')[1]] = obj[key];
-        }
-        else if (key.includes('userType')) {
-            userType = obj[key];
-        }
-        else if (key.includes('accessLevel')) {
-            accessLevel = obj[key];
-        }
-    }
-    return {
-        address,
-        person,
-        userType,
-        accessLevel
-    }
-}
+    for (const objectPath in obj) {
+        const parts = objectPath.split('.');
 
-console.log(jsonConvert(obj));
+        let target = result;
+
+        while (parts.length > 1) {
+            const part = parts.shift();
+            target = target[part] = target[part] || {};
+        }
+        target[parts[0]] = obj[objectPath];
+    };
+
+    return result;
+};
+
+const implementFun = parseNestedJSON(givenObject);
+
+// stringify untuk melihat bentuk object
+console.log(JSON.stringify(implementFun));
